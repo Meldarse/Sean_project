b03d1d03ba1d23c577f4e5adfd5108359c3d9b0c
Meldarse <meldarse@gmail.com>
2023-11-23 00:39:27 -0500

    creacion de characters

---
+++ Gravity/Scripts/Characters/square.gd
@@ -0,0 +1,29 @@
+extends RigidBody2D
+
+var selected: bool = false
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	if selected:
+		freeze = true
+		global_transform.origin = lerp(global_position, get_global_mouse_position(), 30 * delta)
+	else:
+		freeze = false
+		
+
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventScreenTouch:
+		if  event.is_pressed():
+			selected = true
+
+		if event.is_released():
+			selected = false
+
+
+func _on_area_2d_body_entered(body):
+	if body is StaticBody2D:
+		selected = false
--- Gravity/Scripts/Objects/portal.gd
+++ Gravity/Scripts/Objects/portal.gd
@@ -2,18 +2,15 @@ extends Area2D
 
 
 func _ready():
-	hide()
 	$CollisionShape2D.disabled = true
+	$AnimationPlayer.play("Appear")
 
 
 func _process(delta):
 	pass
 
 func _on_timer_timeout():
-		show()
-		$AnimationPlayer.play("Appear")
 		$CollisionShape2D.disabled = false
-		await $AnimationPlayer.animation_finished
 		$AnimationPlayer.play("Idle")
 
 func _on_body_entered(_body):
